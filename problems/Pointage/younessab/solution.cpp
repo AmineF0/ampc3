b794944ea4f02995bf07dda6b2a61a621b6beb0f
younessab12 <younesseaabibi828@gmail.com>
2024-03-05 13:50:58 +0100

    TLE

--- problems/Pointage/younessab/solution.cpp
+++ problems/Pointage/younessab/solution.cpp
@@ -5,5 +5,36 @@ typedef long long ll;
 
 int main(){
     ll m,n;cin>>n>>m;
-    
+    map<string,string> students;
+    for(int i=0;i<n;i++){
+        string code,name;cin>>code>>name;
+        students[code]=name;
+    }
+    map <string,ll> freq;
+    while(m--){
+        ll k;cin>>k;
+        for(int i=0;i<k;i++){
+            string code;cin>>code;
+            freq[code]++;
+        }
+    }
+    vector<ll>nums;
+    for(auto it:freq){
+        nums.push_back(it.second);
+    }
+    sort(nums.begin(),nums.end());
+    ll max = nums[nums.size()-1];
+    vector<string>names;
+    for(auto it:freq){
+        if(it.second==max){
+            names.push_back(students[it.first]);
+        }
+    }
+    if(names.size()==0){
+        cout<<"Something is very wrong"<<endl;
+    }
+    for(auto it:names){
+        cout<<it<<endl;
+    }
+
 }
