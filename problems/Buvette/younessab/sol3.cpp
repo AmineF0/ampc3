02c1cbafc24ea29801ba59c0226a8da404e708f5
younessab12 <younesseaabibi828@gmail.com>
2024-03-05 14:16:34 +0100

    gg

---
+++ problems/Buvette/younessab/sol3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>    
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ll t;cin>>t;
+    while(t--){
+        ll n,m;cin>>n>>m;
+        unordered_map<string,double> items;
+        for(ll i=0;i<n;i++){
+            string s;double p;
+            cin>>s>>p;
+            items[s]=p;
+        }
+        double total=0;
+        for(ll i=0;i<m;i++){
+            string s;cin>>s;
+            ll q;cin>>q;
+            total+=items[s]*q;
+        }
+        cout<<total<<endl;
+    }
+}
