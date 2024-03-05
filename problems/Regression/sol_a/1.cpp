09eba6a1784b4619cbab9693e229e0934dc1a1f4
AmineF0 <aminefirdawsi@gmail.com>
2024-03-05 13:56:45 +0100

    aze

---
+++ problems/Regression/sol_a/1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double x1, x2, y1, y2, x3; cin >> x1 >> x2 >> y1 >> y2 >> x3;
+    // slope of line
+    double m = (y2 - y1) / (x2 - x1);
+    // y-intercept
+    
+}
